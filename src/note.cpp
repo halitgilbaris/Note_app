12627120555868cd37316ba74e641ad54d66b21f
halitgilbaris <bhalitgil250708@gmail.com>
2026-07-22 15:19:39 +0300

    --------------------------------------------------

--- src/note.cpp
+++ src/note.cpp
@@ -150,120 +150,10 @@ void note_class::edit_note(std::vector<Note> &notes){
 
 
 
-void note_class::create_txt(const std::vector<Note> &notes) {
-    static int file_counter = 1;
-    if (notes.empty()) {
-        std::cout << "No notes available to export!\n";
-        return;
-    }
-    int createnot;
-    std::cout << "Please select note for export (1-" << notes.size() << "): ";
-    std::cin >> createnot;
-    std::cin.ignore();
-    
-    if (createnot >= 1 && createnot <= static_cast<int>(notes.size())) {
-        const char* userProfile = std::getenv("USERPROFILE");
-        if (!userProfile) return;
-        
-        std::string desktopPath = std::string(userProfile) + "\\Desktop\\" + std::to_string(file_counter) + "_not.txt";
-        std::ofstream file(desktopPath);
-        
-        if (file.is_open()) {
-            const auto& note = notes[createnot - 1];
-            file << "ID: " << note.id << "\n"
-                << "Title: " << note.title << "\n"
-                << "Content: " << note.content << "\n"
-                << "Created At: " << note.createdAt << "\n";
-            file.close();
-            std::cout << "Note saved to Desktop successfully!\n";
-            file_counter++;
-        }
-    }
-}
-
-
-void note_class::search_note_title(const std::vector<Note> &notes){
-
-    std::string search_title;
-
-    std::cout << "Please enter the title you wish to search for: ";
-    getline(std::cin, search_title);
-
-    for(const auto& titles : notes){
-        if(titles.title == search_title){
-            std::cout << "Note found!\n\n";
-            std::cout << "---------------------------------------------------\n";
-            std::cout << "ID: " << titles.id << "\n";
-            std::cout << "Title: " << titles.title << "\n";
-            std::cout << "Content: " << titles.content << "\n";
-            std::cout << "Created at: " << titles.createdAt << "\n";
-            std::cout << "---------------------------------------------------\n";
-        }
-        else{
-            std::cout << "Note not found!\n";
-        }
-    }
-}
-
-
-
-
-void note_class::search_note_content(const std::vector<Note> &notes){
-
-    std::string search_content;
-
-    std::cout << "Please enter the content you wish to search for: ";
-    getline(std::cin, search_content);
-
-    for(const auto& contents : notes){
-        if(contents.title == search_content){
-            std::cout << "Note found!\n\n";
-            std::cout << "---------------------------------------------------\n";
-            std::cout << "ID: " << contents.id << "\n";
-            std::cout << "Title: " << contents.title << "\n";
-            std::cout << "Content: " << contents.content << "\n";
-            std::cout << "Created at: " << contents.createdAt << "\n";
-            std::cout << "---------------------------------------------------\n";
-        }
-        else{
-            std::cout << "Note not found!\n";
-        }
-    }
-}
-
-
-
-
-
-void note_class::search_note_createdAt(const std::vector<Note> &notes){
-
-    std::string search_createdAt;
-
-    std::cout << "Please enter your secret information when creating the note: ";
-    getline(std::cin, search_createdAt);
-
-    for(const auto& createdAts : notes){
-        if(createdAts.title == search_createdAt){
-            std::cout << "Note found!\n\n";
-            std::cout << "---------------------------------------------------\n";
-            std::cout << "ID: " << createdAts.id << "\n";
-            std::cout << "Title: " << createdAts.title << "\n";
-            std::cout << "Content: " << createdAts.content << "\n";
-            std::cout << "Created at: " << createdAts.createdAt << "\n";
-            std::cout << "---------------------------------------------------\n";
-        }
-        else{
-            std::cout << "Note not found!\n";
-        }
-    }
-}
-
-
-
-
 void note_class::search_menu(const std::vector<Note> &notes){
 
     int choice_search_menu;
+    s_running = true;
 
     while(s_running){
 
@@ -279,13 +169,17 @@ void note_class::search_menu(const std::vector<Note> &notes){
               << "4-Return menu\n"
               << "Your choice: ";
 
-        if(!std::cin >> choice_search_menu){
+        // HATA DÜZELTİLDİ: if içindeki cin kontrolü parantezi düzeltildi
+        if(!(std::cin >> choice_search_menu)){
             std::cout << "Please only number!\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            continue;
         }
 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
 
         switch (choice_search_menu)
         {
